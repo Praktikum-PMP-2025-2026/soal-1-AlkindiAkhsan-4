f8db5b63bdc1695f661a8372865f212d9ac5cd21
AlkindiAkhsan <alkindiakhsan@gmail.com>
2026-04-28 09:12:56 +0700

    soal1.c
    commit sebelum baterai habis

--- soal1.c
+++ soal1.c
@@ -1,5 +1,54 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 3 - Structures and Dynamic Array
+ *   Hari dan Tanggal    : Selasa, 28/04/2026
+ *   Nama (NIM)          : Alkindi Bintang Akhsan (13224022)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : 
+ * 
+ */
 
-int main(void) {
-    return 0;
-}
+
+ #include <stdio.h>
+ #include <string.h>
+
+ void bubblesort(int a[], int size){
+   int temp;
+   for (int i = 0; i<(size-1);i++){
+      for (int j = 0; j<(size-1-i);j++){
+         if(a[j] > a[j+1]) {
+            temp = a[j+1];
+            a[j+1] = a[j];
+            a[j] = temp;
+         }
+      }
+   }
+ }
+  
+ int main(void) {
+   struct rating{
+      char shift[5];
+      int skor;
+      int id;
+      char nama[10];
+   }
+   struct rating rate;
+   int n;
+   scanf("%d", n);
+   for (int i = 0; i<n, i++){
+      scanf("%s", rate.nama);
+      scanf("%d", rate.id);
+      scanf("%s", rate.shift);
+      scanf("%d", rate.skor);
+   }
+   bubblesort(rate.skor[], n);
+   if (rate.skor[0] == rate.skor[1]){
+      bubblesort(rate.id[], n);
+   }
+   if (rate.shift == "PAGI"){
+      printf("PAGI %s %d %d", rate.nama[0] rate.skor[0] rate.id[0]);
+   }
+   return 0;
+ }
+ 
+
+ 
